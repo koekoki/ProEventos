c3bb85df37b9b1db9fef23a82dd6a6cce37ea5ca
Koekoki <kaiky.br34@gmail.com>
2022-05-30 14:45:38 -0300

    Seção 3: Angular

---
+++ back/src/ProEvento.API/Controllers/EventosController.cs
@@ -0,0 +1,65 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using ProEvento.API.Data;
+using ProEvento.API.Model;
+
+namespace ProEvento.API.Controllers
+{
+    [ApiController]
+    [Route("api/[controller]")]
+    public class EventosController : ControllerBase
+    {
+        private readonly DataContext _context;
+        public EventosController(DataContext context)
+        {
+            _context = context;
+
+        }
+
+        [HttpGet]
+        public IEnumerable<Evento> Get()
+        {
+            return _context.Eventos;
+        }
+
+        [HttpGet("{id}")]
+        [ProducesResponseType(StatusCodes.Status400BadRequest)]
+        [ProducesResponseType(StatusCodes.Status200OK)]
+        public async Task<IActionResult> GetById(int id)
+        {
+            Evento eventoEncontrado = _context.Eventos.FirstOrDefault(evento => evento.eventoId.Equals(id));
+            if (eventoEncontrado == null)
+            {
+                return BadRequest("Evento não encontrado");
+            }
+            else
+            {
+                return Ok(eventoEncontrado);
+            }
+        }
+
+        [HttpPost]
+        public string Post()
+        {
+            return "POST";
+        }
+
+
+        [HttpPut("{id}")]
+        public string Put(int id)
+        {
+            return $"Put = {id}";
+        }
+
+
+        [HttpDelete("{id}")]
+        public string Delete(int id)
+        {
+            return $"delete = {id}";
+        }
+    }
+}
